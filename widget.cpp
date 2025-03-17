98e6415ad2461b598aeeabfbf71c19c102d3a4d6
Taeho-Yoon1 <requiemtedy@gmail.com>
2025-03-17 11:15:08 -0400

    setting

---
+++ widget.cpp
@@ -0,0 +1,61 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    //socket_.connected();
+    QObject::connect(&socket_, &QAbstractSocket::connected, this, &Widget::doConnected);
+    QObject::connect(&socket_, &QAbstractSocket::disconnected, this, &Widget::doDisConnected);
+    QObject::connect(&socket_, &QIODevice::readyRead, this, &Widget::doReadyread);
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::doConnected(){
+
+    ui->pteMessgae->insertPlainText("Connected");
+
+}
+
+void Widget::doDisConnected(){
+
+    ui->pteMessgae->insertPlainText("DisConnected");
+
+}
+
+void Widget::doReadyread(){
+
+    ui->pteMessgae->insertPlainText(socket_.readAll());
+}
+
+void Widget::on_pbConnect_clicked()
+{
+    socket_.connectToHost(ui->LEhost->text(), ui->LEPort->text().toUShort());
+
+}
+
+
+void Widget::on_pbDisConnect_clicked()
+{
+    socket_.disconnectFromHost();
+}
+
+
+void Widget::on_Clear_clicked()
+{
+    ui-> pteMessgae ->clear();
+}
+
+
+void Widget::on_pbSend_clicked()
+{
+    socket_.write(ui->pteSend->toPlainText().toUtf8());
+}
+
---
+++ widget.h
@@ -0,0 +1,42 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTcpSocket>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+    //QTcpSocket socket_;
+    QSslSocket socket_;
+
+public slots :
+    void doConnected();
+    void doDisConnected();
+    void doReadyread();
+
+
+private slots:
+    void on_pbConnect_clicked();
+
+    void on_pbDisConnect_clicked();
+
+    void on_Clear_clicked();
+
+    void on_pbSend_clicked();
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
